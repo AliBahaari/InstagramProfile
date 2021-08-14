4529360d8763aeed7589f381e8d621c216994b4b
Ali Bahaari <alibahaari1997@gmail.com>
2021-08-14 06:08:09 +0430

    Completed

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests as req, re, requests_random_user_agent
+from bs4 import BeautifulSoup
+from io import BytesIO
+from PIL import Image
+
+
+class InstagramProfilePicture:
+
+
+  def __init__(self, username):
+      self.username = username
+
+
+  def show_profile_picture(self):
+
+    # Request to Instagram
+    request_result = req.get(f'https://www.instagram.com/{self.username}/')
+
+    # Search in Data
+    soup = BeautifulSoup(request_result.text, 'html.parser')
+    body_tags = soup.find_all('body')
+    profile_picture_url = re.findall(r"profile_pic_url_hd\":\"([\S]+?)\"", str(body_tags[0]))[0].replace(r'\u0026', '&')
+
+    # Show the Image
+    profile_picture_bytes = req.get(profile_picture_url).content
+    profile_picture = Image.open(BytesIO(profile_picture_bytes))
+    profile_picture.show()
+
+
+IGPP = InstagramProfilePicture('<USERNAME>')
+IGPP.show_profile_picture()

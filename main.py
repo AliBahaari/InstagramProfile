b0e4f1ab99e578d7feb63a16eab0184ad8a62439
Ali Bahaari <alibahaari1997@gmail.com>
2021-08-16 17:00:36 +0430

    Added Biography

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 from PIL import Image
 
 
+print('Warning: Neither Proxy Nor VPN!')
+
+
 class InstagramProfilePicture:
 
 
@@ -11,15 +14,15 @@ class InstagramProfilePicture:
 
 
   def show_profile_picture(self):
-    print('Warning: Don\'t Behind Any Proxy Or VPN.')
-
     # Request to Instagram
     request_result = req.get(f'https://www.instagram.com/{self.username}/')
 
     # Search in Data
     soup = BeautifulSoup(request_result.text, 'html.parser')
     body_tags = soup.find_all('body')
+
     profile_picture_url = re.findall(r"profile_pic_url_hd\":\"([\S]+?)\"", str(body_tags[0]))[0].replace(r'\u0026', '&')
+    profile_biography = re.findall(r"biography\":\"(.*?)\"", str(body_tags[0]))[0]
 
     # Show the Image
     profile_picture_bytes = req.get(profile_picture_url, stream=True).raw
@@ -27,5 +30,18 @@ class InstagramProfilePicture:
     profile_picture.show()
 
 
-IGPP = InstagramProfilePicture('USERNAME')
-IGPP.show_profile_picture()
+  def show_profile_biography(self):
+    # Request to Instagram
+    request_result = req.get(f'https://www.instagram.com/{self.username}/')
+
+    # Search in Data
+    soup = BeautifulSoup(request_result.text, 'html.parser')
+    body_tags = soup.find_all('body')
+
+    # Show the Biography
+    profile_biography = re.findall(r"biography\":\"(.*?)\"", str(body_tags[0]))[0]
+    print(profile_biography)
+
+
+IGPP = InstagramProfilePicture('leomessi')
+IGPP.show_profile_biography()
